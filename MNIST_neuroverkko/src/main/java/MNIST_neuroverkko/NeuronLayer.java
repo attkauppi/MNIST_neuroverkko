78bd38b81050e0ebeb69538a03e4aaa29e649992
Ari Kauppi <ari.kauppi@santelo.fi>
2021-04-03 01:29:31 +0300

    Abstraktin NeuronLayer-luokan toiminnoista valtaosa implementoitu

--- MNIST_neuroverkko/src/main/java/MNIST_neuroverkko/NeuronLayer.java
+++ MNIST_neuroverkko/src/main/java/MNIST_neuroverkko/NeuronLayer.java
@@ -6,6 +6,7 @@
 package MNIST_neuroverkko;
 
 import java.util.ArrayList;
+import math.IActivationFunction;
 
 /**
  * Yleisimplementaatio NeuralLayerista. Kaikki näille kerroksille yhteiset
@@ -24,5 +25,38 @@ public abstract class NeuronLayer {
     // TODO: Korvaa ArrayList omalla implementaatiolla.
     private ArrayList<Neuron> neuron;
     
+    /**
+     * Kerroksen käyttämän aktivaatiofunktion määrittely.
+     */
+    // TODO: Implementoi aktivaatiofunktiot.
+    protected IActivationFunction activationFunction;
     
+    /**
+     * Edellinen kerros, jonka output tulee tähän kerrokseen. Auttaa pitämään
+     * lukua kerrosten järjestyksestä.
+     */
+    protected NeuronLayer prevLayer;
+    
+    /**
+     * Seuraava kerros, johon tämän kerroksen arvot menevät inputteina.
+     */
+    protected NeuronLayer nextLayer;
+    
+    /**
+     * Lista input arvoja, jotka tämä kerros saa syötteinä.
+     */
+    // TODO: Korvaa ArrayList omalla implementaatiolla.
+    protected ArrayList<Double> input;
+    
+    /**
+     * lista output-arvoja, jotka tämä kerros tuottaa
+     */
+    // TODO: Korvaa ArrayList omalla implementaatiolla.
+    protected ArrayList<Double> output;
+    
+    /**
+     * Inputtien lukumäärä, jonka tämä keros voi ottaa vastaan
+     */
+    protected int numberOfInputs;
+
 }
